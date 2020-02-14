96352b5120091b77ead790e82861c64070fe414a
Alex Petroni <alexpetroni@gmail.com>
2020-02-14 16:46:31 +0100

    main menu

--- admin/src/components/MainMenu.vue
+++ admin/src/components/MainMenu.vue
@@ -1,124 +1,40 @@
 <template>
   <v-list dense>
-     <template v-for="item in items">
-       <v-row
-         v-if="item.heading"
-         :key="item.heading"
-         align="center"
-       >
-         <v-col cols="6">
-           <v-subheader v-if="item.heading">
-             {{ item.heading }}
-           </v-subheader>
-         </v-col>
-         <v-col
-           cols="6"
-           class="text-center"
-         >
-           <a
-             href="#!"
-             class="body-2 black--text"
-           >EDIT</a>
-         </v-col>
-       </v-row>
-
-
-       <v-list-item-group
-
-         :key="item.route"
-         v-model="kk"
-         :prepend-icon="item.model ? item.icon : item['icon-alt']"
-         append-icon=""
-       >
-         <!-- <template v-slot:activator v-if="item.children">
-           <v-list-item-content @click="navigateTo(item.route)">
-             <v-list-item-title>
-               {{ item.text }}
-             </v-list-item-title>
-           </v-list-item-content>
-
-         <v-list-item
-           v-for="(child, i) in item.children"
-           :key="i"
-           link
-         >
-           <v-list-item-action v-if="child.icon">
-             <v-icon>{{ child.icon }}</v-icon>
-           </v-list-item-action>
-           <v-list-item-content>
-             <v-list-item-title>
-               {{ child.text }}
-             </v-list-item-title>
-           </v-list-item-content>
-         </v-list-item>
-       </template> -->
-
-
-         <v-list-item
-           :key="item.text"
-           link
-         >
-           <v-list-item-action>
-             <v-icon>{{ item.icon }}</v-icon>
-           </v-list-item-action>
-           <v-list-item-content  @click="navigateTo(item.route)">
-             <v-list-item-title>
-               {{ item.text }}
-             </v-list-item-title>
-           </v-list-item-content>
-         </v-list-item>
-
-
-       </v-list-item-group>
-     </template>
+  <v-list-item-group v-model="model">
+    <v-list-item
+      v-for="(item, i) in items"
+      :key="i"
+      @click="navigateTo(item.route)"
+    >
+      <v-list-item-icon>
+        <v-icon v-text="item.icon"></v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list-item-group>
    </v-list>
 </template>
 
 <script>
 
 export default {
-  name: '',
-
-  components: {
-
-  },
-
-  directives: {
-
-  },
-
-  filters: {
-
-  },
-
-  extends: {
-
-  },
-
-  mixins: {
-
-  },
-
-  model: {
-
-  },
-
-  props: {
-
-  },
+  name: 'MainMenu',
 
   data () {
     return {
-      kk: 0,
+      model: null,
 
       items: [
-        { icon: 'mdi-contacts', text: 'Ships', route: 'ships', heading: 'xxxx' },
-        { icon: 'mdi-history', text: 'Cruises', route: 'cruises' },
-        { icon: 'mdi-content-copy', text: 'Itineraries', route: 'itineraries' },
-        { icon: 'mdi-message', text: 'Media', route: 'media' },
-        { icon: 'mdi-message', text: 'Orders', route: 'orders' },
-        { icon: 'mdi-message', text: 'Clients', route: 'clients' },
-        { icon: 'mdi-settings', text: 'Settings', route: 'settings' },
+        { icon: 'mdi-view-dashboard-outline', text: 'Dashboard', route: 'home'},
+        { icon: 'mdi-ship-wheel', text: 'Ships', route: 'ships' },
+        { icon: 'mdi-map-clock-outline', text: 'Cruises', route: 'cruises' },
+        { icon: 'mdi-map-marker-path', text: 'Itineraries', route: 'itineraries' },
+        { icon: 'mdi-image-multiple', text: 'Media', route: 'media' },
+        { icon: 'mdi-cash-multiple', text: 'Orders', route: 'orders' },
+        { icon: 'mdi-account-multiple-outline', text: 'Clients', route: 'clients' },
+        { icon: 'mdi-settings-outline', text: 'Settings', route: 'settings' },
         // {
         //   icon: 'mdi-chevron-up',
         //   'icon-alt': 'mdi-chevron-down',
@@ -170,8 +86,3 @@ export default {
 
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
