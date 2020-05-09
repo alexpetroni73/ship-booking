c263237e9433d4f5910e80d98c405eee866ebe8e
alex <alexpetroni73@gmail.com>
2020-05-09 12:11:42 +0200

    added front

---
+++ front/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    <v-toolbar-title>My Logo</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+        <router-view></router-view>
+    </v-content>
+    <v-footer
+    color="primary"
+  app
+>
+  <span class="white--text">&copy; {{ (new Date()).getFullYear() }}</span>
+</v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
