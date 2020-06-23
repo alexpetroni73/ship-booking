55cbf9227a19c9b2f0a2bd921faedcb09ca0ba3e
alex <alexpetroni73@gmail.com>
2020-06-23 14:40:33 +0200

    just

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,6 @@ const imagekit = require('./../image-kit')
 function wireRoutes (app) {
   app.get(process.env.SERVER_AUTH_PATH_IMGKIT, function(req, res){
     const auth = imagekit.getAuthenticationParameters()
-    console.log('auth %o', auth)
     res.send(auth)
   })
 
